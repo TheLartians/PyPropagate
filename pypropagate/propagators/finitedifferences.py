433122359eaaf4c24a07ab25ccad6f770cc26dc0
Lars <lars.melchior@gmail.com>
2016-02-08 18:11:01 +0100

    import global module

--- pypropagate/propagators/finitedifferences.py
+++ pypropagate/propagators/finitedifferences.py
@@ -1,6 +1,6 @@
 
 from .propagator import Propagator
-from .._pypropagate import finite_difference_1D,finite_difference_2D
+from _pypropagate import finite_difference_1D,finite_difference_2D
 from ..coordinate_ndarray import CoordinateNDArray
 import expresso.pycas as pc
 import numpy as np
