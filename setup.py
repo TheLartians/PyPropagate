bbf8b3f291695739e5413baefd3d1fd1531cebe0
Lars <lars.melchior@gmail.com>
2016-02-23 11:40:27 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,16 @@ setup(
     name='pypropagate',
     version='1.0.3',
     description='A python based paraxial wave propagation framework',
-    
+
     author='Lars Melchior',
     author_email='lars.melchior@gmail.com',
 
     packages=find_packages(exclude=['tests*']),
-    
-    install_require={
+
+    install_requires={
         'matplotlib',
         'numpy',
-        'expresso[pyplot]'
+        'expresso[pycas]'
     },
 
     zip_safe=False,
@@ -26,9 +26,9 @@ setup(
 
     ext_modules=[
         Extension('_pypropagate',
-                  sources = ['source/crank_nicolson.cpp','source/finite_difference.cpp','source/python.cpp'],
-                  include_dirs=['libs',numpy.get_include()], 
-                  libraries=['boost_python'], 
+                  sources = ['source/finite_difference.cpp','source/python.cpp'],
+                  include_dirs=['libs',numpy.get_include()],
+                  libraries=['boost_python'],
                   library_dirs=['/'],
                   extra_compile_args=['-g','-std=c++11','-Wno-unknown-pragmas','-ffast-math','-O3']
                   ),
