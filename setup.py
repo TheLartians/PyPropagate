1692e11c7374ea314095bbce490de6f8851d6f35
Lars Melchior <TheLartians@users.noreply.github.com>
2017-08-01 19:13:55 +0200

    update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import numpy
 
 setup(
     name='pypropagate',
-    version='1.2.1',
+    version='1.2.2',
     description='A python based paraxial wave propagation framework',
 
     author='Lars Melchior',
